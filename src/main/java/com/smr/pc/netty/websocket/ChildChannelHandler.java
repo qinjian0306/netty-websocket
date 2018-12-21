86ba5b4b1a41cb7564f19136e791e670256aa5b8
qinjian <qinjian0306@163.com>
2018-12-21 18:13:33 +0800

    a

---
+++ src/main/java/com/smr/pc/netty/websocket/ChildChannelHandler.java
@@ -0,0 +1,59 @@
+package com.smr.pc.netty.websocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * 自定义管道Handler类
+ *
+ * @author QJ
+ * @date 2018/12/21
+ *
+ */
+public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel e) throws Exception {
+
+        //websocket协议本身是基于http协议的，所以这边也要使用http解编码器
+        e.pipeline().addLast("http-codec",new HttpServerCodec());
+
+        //netty是基于分段请求的，HttpObjectAggregator的作用是将请求分段再聚合,参数是聚合字节的最大长度
+        e.pipeline().addLast("aggregator",new HttpObjectAggregator(65536));
+
+        //以块的方式来写的处理器
+        e.pipeline().addLast("http-chunked",new ChunkedWriteHandler());
+
+        //ws://server:port/context_path
+        //ws://localhost:9999/ws
+        //参数指的是contex_path
+        e.pipeline().addLast(new WebSocketServerProtocolHandler("/spotws"));
+
+        //websocket定义了传递数据的6中frame类型
+//        e.pipeline().addLast(new TextWebSocketFrameHa());
+
+        // 1.
+        // 2.
+        // 3.
+        // 4.
+        // 5.
+        // 6.
+
+
+
+
+
+
+
+
+
+        // 自定义的handler
+        e.pipeline().addLast("handler",new MyWebSocketServerHandler());
+    }
+
+}
