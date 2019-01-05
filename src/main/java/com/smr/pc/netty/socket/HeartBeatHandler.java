b47a176f01bb636859bd6baa2023659148bc21b7
qinjian <qinjian0306@163.com>
2019-01-05 11:07:28 +0800

    心跳

--- src/main/java/com/smr/pc/netty/socket/HeartBeatHandler.java
+++ src/main/java/com/smr/pc/netty/socket/HeartBeatHandler.java
@@ -1,38 +1,67 @@
 package com.smr.pc.netty.socket;
 
-import io.netty.channel.ChannelHandlerAdapter;
-import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.timeout.IdleStateEvent;
 
 /**
- * 心跳检测Handler
+ * HeartBeat Handler
  *
  * @author QJ
- * @date 2018/12/22
+ * @date 2018/12/24
  */
 public class HeartBeatHandler extends ChannelHandlerAdapter {
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if (evt instanceof IdleStateEvent) {
             IdleStateEvent event = (IdleStateEvent) evt;
+
             String eventType = null;
             switch (event.state()) {
                 case READER_IDLE:
-                    eventType = "读空闲";
+                    eventType = "read idle";
                     break;
                 case WRITER_IDLE:
-                    eventType = "写空闲";
+                    eventType = "write idle";
+//                    pong(ctx);
                     break;
                 case ALL_IDLE:
-                    eventType = "读写空闲";
+                    eventType = "all idle";
+//                    close(ctx);
                     break;
+                default:
             }
 
-            System.out.println(ctx.channel().remoteAddress() + "超时事件: " + eventType);
-
-            // 关闭
-            ctx.channel().close();
-
+        } else {
+            super.userEventTriggered(ctx, evt);
         }
     }
+
+    /**
+     * 发送心跳
+     *
+     * @param ctx
+     */
+    private void pong(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(new TextWebSocketFrame("pong:" + System.currentTimeMillis()));
+    }
+
+    /**
+     * 关闭连接
+     *
+     * @param ctx
+     */
+    private void close(ChannelHandlerContext ctx) {
+        ChannelFuture cf = ctx.writeAndFlush(new TextWebSocketFrame("Timeout! Connection will close"));
+      /*  cf.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                ctx.channel().close();
+            }
+        });*/
+
+        cf.addListener(ChannelFutureListener.CLOSE);
+    }
+
+
 }
